27667442f806e3817c3d35d70e8314a526f48df2
Somyot Thanimwas <plaifaeng@gmail.com>
2026-09-10 07:56:42 +0700

    Add Company Profile route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import CompanyProfile from "./pages/CompanyProfile";
 import Home from "./pages/Home";
 
 
@@ -11,6 +12,7 @@ function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/company-profile"} component={CompanyProfile} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
